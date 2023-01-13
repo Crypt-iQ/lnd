12b5ed578d304a64b4ce70e137abd2e90b44f8d4
eugene <elzeigel@gmail.com>
2023-01-13 10:49:20 -0500

    htlcswitch+peer: add Shutdown retransmission logic to the link
    This commit allows the link-construction function addLink in
    peer/brontide.go to set a boolean RetransmitShutdown to true. When
    this is true, the link will retransmit Shutdown using the persisted
    delivery script. This happens right after the ChannelReestablish
    dance. When the peer/brontide.go sets this boolean to true, it creates
    a ChanCloser and feeds an identical Shutdown message to the ChanCloser
    so that it can advance state. A link-level test is included that
    exercises the retransmission logic.

--- htlcswitch/link.go
+++ htlcswitch/link.go
@@ -329,6 +329,12 @@ type ChannelLinkConfig struct {
 	// is called. It takes a quit chan that is used as a precaution to
 	// avoid deadlock.
 	NotifyCoopReady func(chanPoint *wire.OutPoint, quit chan struct{})
+
+	// RetransmitShutdown is a boolean that indicates whether the link
+	// should retransmit a Shutdown during the Reestablish flow. This
+	// automatically starts the link in "shutdown mode" so that new HTLCs
+	// are not accepted or sent.
+	RetransmitShutdown bool
 }
 
 // channelLink is the service which drives a channel's commitment update
@@ -816,6 +822,26 @@ func (l *channelLink) syncChanStates() error {
 			if err != nil {
 				return fmt.Errorf("unable to re-send "+
 					"FundingLocked: %v", err)
+			}
+		}
+
+		// If we have been signaled to retransmit Shutdown, we'll do so
+		// here.
+		if l.cfg.RetransmitShutdown {
+			// Recreate the Shutdown message.
+			shutdownMsg := lnwire.NewShutdown(
+				l.ChanID(),
+				l.channel.LocalUpfrontShutdownScript(),
+			)
+
+			// Flip the shutdownInit and shutdownSent bools.
+			l.shutdownInit = true
+			l.shutdownSent = true
+
+			err = l.cfg.Peer.SendMessage(false, shutdownMsg)
+			if err != nil {
+				return fmt.Errorf("unable to re-send "+
+					"Shutdown: %v", err)
 			}
 		}
 
